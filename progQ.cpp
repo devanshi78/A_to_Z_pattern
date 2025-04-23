29f9e667cc5daab90a474e6cb848e0a551813b2b
devanshi <parekhdevanshi76@gmail.com>
2025-04-23 22:02:15 +0530

    update

---
+++ progQ.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 1;i <= 5;i++)
+    {
+        for(int j = 1;j <= 5;j++)
+        {
+            if((i == 1 && j == 2) || (i == 1 && j == 3)|| (i == 4 && j == 2) || (i == 4 && j == 3) || (i == 2 && j == 1) || (i == 3 && j == 1) || (i == 2 && j == 4) || (i == 3 && j == 4) || (i == 3 && j == 3) || (i == 4 && j == 4) || (i == 5 && j == 5))
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
---
+++ progU.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= 5; j++)
+        {
+            if((j == 1 && i <= 4) || (j == 5 && i <= 4) || (j == 2 && i == 5 || j == 3 && i == 5 || j == 4 && i == 5))
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
